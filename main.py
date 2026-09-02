308968b73b54c36fde5eed4445b59ed5f9017a7c
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2026-09-02 16:06:58 +0530

    meachine task

--- main.py
+++ main.py
@@ -1,6 +1,67 @@
-def main():
-    print("Hello from ai-machine-test!")
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+groq_client = Groq()
+
+croma_client = chromadb.PersistentClient()
+
+collection = croma_client.get_or_create_collection(name="my_collection")
 
 
-if __name__ == "__main__":
-    main()
+pdf = PyPDFLoader(file_path="Muhammed_Riswan_P (V1).pdf")
+pages = pdf.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+
+chunks = text_splitter.split_text(pages)
+# print(chunks)
+
+for i,chunk in enumerate(chunks):
+    collection.add(
+    ids=[f"id{i}"],
+    documents=[chunks],
+)
+    while True:
+        print("Type EXIT to exit")
+        user = input("Enter the question")
+
+        if user == "EXIT":
+            break
+
+        context = collection.query(
+    query_texts=[user]
+)
+
+        chat_completion = groq_client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        
+        {
+            "role": "user",
+            "content": f"from this context \n Context={context} answer this question {user}",
+        }
+    ],
+
+    
+    model="openai/gpt-oss-120b"
+)
+
+
+print(chat_completion.choices[0].message.content)
+
+
+    
+ 
+
+
